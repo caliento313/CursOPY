405f50eac478da318bca792b74550c5fb71164f4
Andre Caliento <andrecaliento@gmail.com>
2025-09-16 16:20:29 -0300

    Update CadastroProdutos.py

--- CadastroProdutos.py
+++ CadastroProdutos.py
@@ -1,15 +1,10 @@
 import time
 from multiprocessing.reduction import send_handle
-
 import pyautogui
 import pandas
 from time import sleep
 #defina o tempo de espera entre as aberturas dos comandos pyautogui
-
 pyautogui.PAUSE = 0.7
-
-
-
 # abrir sistema
 pyautogui.press('win')
 pyautogui.write('microsoft edge')
@@ -19,7 +14,6 @@ pyautogui.press('enter')
 time.sleep(3)
 #print(pyautogui.position())
 #cadastrar senha
-
 pyautogui.click(x=32, y=312)
 pyautogui.write('andrecaliento@gmail.com')
 pyautogui.press('tab')
@@ -29,12 +23,9 @@ pyautogui.press('enter')
 sleep(3)
 #cadstrar produtos
 tabela = pandas.read_csv('produtos.csv')
-
 for linha in tabela.index:
-
     pyautogui.click(x=57, y=256)
     #print(tabela)
-
     codigo = tabela.loc[ linha, 'codigo']
     pyautogui.write(codigo)
     pyautogui.press('tab')
